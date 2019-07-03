3e7c407f43e13c731071aef916a75b6a30fdb4b3
Julian Goldsmith <julian@juliangoldsmith.com>
2019-07-02 21:42:48 -0400

    WIP

--- src/main.rs
+++ src/main.rs
@@ -20,22 +20,30 @@ pub struct Ray {
 }
 
 impl Ray {
-    pub fn point_at(&self, dist: f32) -> Vector3<f32> {
+    pub fn point_at_distance(&self, dist: f32) -> Vector3<f32> {
         self.origin + self.direction * dist
     }
 }
 
-// P(t) * P(t) = radius ^ 2 -- in local coordinates
-// We want to find t
-// dot(P - center, P - center) = radius ^ 2
+#[derive(Copy, Clone)]
+pub struct Intersection {
+    pub pos: Vector3<f32>,
+    pub dist: f32,
+    pub normal: Vector3<f32>,
+}
+
+pub trait Intersect {
+    fn intersect(&self, r: &Ray) -> Option<Intersection>;
+}
+
 #[derive(Copy, Clone)]
 pub struct Sphere {
     pub center: Vector3<f32>,
     pub radius: f32,
 }
 
-impl Sphere {
-    pub fn hit(&self, r: &Ray) -> Option<f32> {
+impl Intersect for Sphere {
+    fn intersect(&self, r: &Ray) -> Option<Intersection> {
         let r_origin_local = r.origin - self.center;
         let a = r.direction.dot(r.direction);
         let b = 2.0 * r_origin_local.dot(r.direction);
@@ -43,57 +51,72 @@ impl Sphere {
         let discriminant = b * b - 4.0 * a * c;
 
         if discriminant > 0.0 {
-            Some((-b - discriminant.sqrt()) / (2.0 * a))
+            let dist = if a < 0.0 {
+                -b + discriminant.sqrt()
+            } else {
+                -b - discriminant.sqrt()
+            } / (2.0 * a);
+
+            let pos = r.point_at_distance(dist);
+            let intersection = Intersection {
+                pos,
+                dist,
+                normal: (pos - self.center) / self.radius,
+            };
+
+            Some(intersection)
         } else {
             None
         }
     }
 }
 
-fn color(r: &Ray) -> Vector3<f32> {
-    let unit_direction = r.direction.normalize();
-    let t = 0.5 * (unit_direction.y + 1.0);
-    (1.0 - t) * Vector3::new(1.0, 1.0, 1.0) + t * Vector3::new(0.5, 0.7, 1.0)
+pub struct World {
+    pub spheres: Vec<Sphere>,
 }
 
-fn render(width: usize, height: usize) -> Vec<u8> {
-    let mut pixels = Vec::new();
-    let aspect = (height as f32) / (width as f32);
-    let origin = Vector3::zero();
-    let screen_space = Vector2::new(4.0, 4.0 * aspect);
-    let lower_left_corner = (screen_space / -2.0).extend(-1.0);     // FIXME: Go from top left.
+impl World {
+    fn render(&self, width: usize, height: usize) -> Vec<u8> {
+        let mut pixels = Vec::new();
+        let aspect = (height as f32) / (width as f32);
+        let origin = Vector3::zero();
+        let screen_space = Vector2::new(4.0, 4.0 * aspect);
+        let lower_left_corner = (screen_space / -2.0).extend(-1.0);     // FIXME: Go from top left.
 
-    let s = Sphere {
-        center: Vector3::new(0.0, 0.0, -1.0),
-        radius: 0.5,
-    };
+        let screen_space_transform = Matrix2::new(
+            screen_space.x / (width as f32), 0.0,
+            0.0, screen_space.y / (height as f32));
 
-    let screen_space_transform = Matrix2::new(screen_space.x / (width as f32), 0.0, 0.0, screen_space.y / (height as f32));
+        for y in 0..height {
+            for x in 0..width {
+                let uv = screen_space_transform * Vector2::new(x as f32, y as f32);
 
-    for y in 0..height {
-        for x in 0..width {
-            let uv = screen_space_transform * Vector2::new(x as f32, y as f32);
+                let r = Ray {
+                    origin,
+                    direction: lower_left_corner + uv.extend(0.0),
+                };
 
-            let r = Ray {
-                origin,
-                direction: lower_left_corner + uv.extend(0.0),
+                let curr_ixn = self.spheres.iter().
+                    map(|s| s.intersect(&r)).
+                    filter(|ixn| ixn.is_some()).
+                    map(|ixn| ixn.unwrap()).
+                    min_by(|a, b| a.dist.partial_cmp(&b.dist).unwrap());
+
+                let color = 255.0 * match curr_ixn {
+                    Some(ixn) => {
+                        0.5 * (ixn.normal + Vector3::new(1.0, 1.0, 1.0))
+                    },
+                    None => Vector3::new(0.1, 0.4, 0.1),
+                };
+
+                pixels.push(color.x as u8);
+                pixels.push(color.y as u8);
+                pixels.push(color.z as u8);
             };
-
-            let color = 255.0 * match s.hit(&r) {
-                Some(dist) => {
-                    let normal = (r.point_at(dist) - s.center).normalize();
-                    0.5 * (normal + Vector3::new(1.0, 1.0, 1.0))
-                },
-                None => color(&r),
-            };
-
-            pixels.push(color.x as u8);
-            pixels.push(color.y as u8);
-            pixels.push(color.z as u8);
         };
-    };
 
-    pixels
+        pixels
+    }
 }
 
 fn write_png(path: &Path, data: &[u8], width: usize, height: usize) {
@@ -112,7 +135,23 @@ fn main() {
     let height = 700;
     let path = Path::new("/var/www/rays.png");
 
-    let data = render(width, height);
+    let world = World {
+        spheres: vec![
+            Sphere {
+                center: Vector3::new(0.0, 0.0, -2.0),
+                radius: 0.5,
+            },
+            Sphere {
+                center: Vector3::new(1.0, 0.0, -1.5),
+                radius: 0.5,
+            },
+            Sphere {
+                center: Vector3::new(-1.0, 0.0, -1.5),
+                radius: 0.5,
+            },
+        ],
+    };
+    let data = world.render(width, height);
 
     write_png(&path, &data, width, height);
 }
