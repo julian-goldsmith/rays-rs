28e3508f46adf7e6b5b3588981d928a006e8e9fa
Julian Goldsmith <julian@juliangoldsmith.com>
2019-07-08 17:54:55 -0400

    WIP

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use cgmath::prelude::*;
 use cgmath::{Matrix4, Point3, Vector2, Vector3, Vector4};
 use png::HasParameters;
 use rand::Rng;
-use rand_distr::{Distribution, Normal, UnitSphere};
+use rand_distr::{Distribution, UnitSphere};
 
 // https://nelari.us/post/raytracer_with_rust_and_zig/
 // https://www.cl.cam.ac.uk/teaching/1999/AGraphHCI/SMAG/node2.html
@@ -73,12 +73,10 @@ pub struct World {
 
 impl World {
     fn render(&self, width: usize, height: usize) -> Vec<u8> {
-        let num_samples = 4;
         let mut pixels = Vec::new();
         let mut rng = rand::thread_rng();
-        let ndist = Normal::new(0.0, 0.25).unwrap();                   // TODO: Adjust.
 
-        let origin = Point3::new(0.0, 0.0, 0.0);
+        let origin = Point3::new(0.0, 0.0, 0.0);                        // FIXME: Origin doesn't work properly.
 
         let aspect = (height as f32) / (width as f32);
         let sst = Matrix4::new(
@@ -87,17 +85,13 @@ impl World {
             0.0, 0.0, 1.0, 0.0,
             -0.5, -0.5, 0.0, 1.0);
 
-        let persp = cgmath::frustum(aspect * -0.5, aspect * 0.5, -0.5, 0.5, 0.1, 10.0);
+        let persp = cgmath::frustum(aspect * -0.5, aspect * 0.5, -0.5, 0.5, 0.1, 100.0);
         let view = sst * Matrix4::look_at(origin, Point3::new(0.0, 0.0, -1.0), Vector3::new(0.0, 1.0, 0.0));
 
         for y in (0..height).rev() {
             for x in 0..width {
                 let uv = Vector2::new(x as f32, y as f32);
-
-                let color: Vector3<f32> = (0..num_samples).
-                    fold(Vector3::zero(),
-                         |acc, _| acc + self.sample(&mut rng, &ndist, origin, uv, &persp, &view)) *
-                    255.0 / (num_samples as f32);
+                let color = self.sample(&mut rng, origin, uv, &persp, &view) * 255.0;
 
                 pixels.push(color.x as u8);
                 pixels.push(color.y as u8);
@@ -108,28 +102,34 @@ impl World {
         pixels
     }
 
-    fn sample(&self, rng: &mut impl Rng, dist: &impl Distribution<f32>,
-              origin: Point3<f32>, uv: Vector2<f32>, persp: &Matrix4<f32>,
-              view: &Matrix4<f32>) -> Vector3<f32> {
-        let jitter = Vector2::new(dist.sample(rng), dist.sample(rng));
-        let sample_uv = Vector4::new(uv.x + jitter.x, uv.y + jitter.y, 0.0, 1.0);
-        let direction = (persp * view * sample_uv).truncate().normalize();
+    fn sample(&self, rng: &mut impl Rng, origin: Point3<f32>, uv: Vector2<f32>,
+              persp: &Matrix4<f32>, view: &Matrix4<f32>) -> Vector3<f32> {
+        let num_samples = 4;
+        let mut color = Vector3::zero();
 
-        let r = Ray { origin, direction };
+        for _ in 0..num_samples {
+            let jitter = Vector3::<f32>::from(UnitSphere.sample(rng)).truncate();
+            let sample_uv = (uv + jitter).extend(0.0).extend(1.0);
+            let direction = (persp * view * sample_uv).truncate().normalize();
 
-        let curr_ixn = self.spheres.iter().
-            filter_map(|s| s.intersect(&r)).
-            min_by(|a, b| a.dist.partial_cmp(&b.dist).unwrap());
+            let r = Ray { origin, direction };
 
-        match curr_ixn {
-            Some(ixn) => {
-                let bounce: Vector3<f32> = UnitSphere.sample(rng).into();
-                let target = ixn.pos + ixn.normal + bounce;
+            let curr_ixn = self.spheres.iter().
+                filter_map(|s| s.intersect(&r)).
+                min_by(|a, b| a.dist.partial_cmp(&b.dist).unwrap());
 
-                0.5 * ((ixn.pos - target).normalize() + Vector3::new(1.0, 1.0, 1.0))
-            },
-            None => Vector3::new(0.1, 0.4, 0.1),
-        }
+            color += match curr_ixn {
+                Some(ixn) => {
+                    let bounce: Vector3<f32> = UnitSphere.sample(rng).into();
+                    let target = ixn.pos + ixn.normal + bounce;
+
+                    0.5 * ((ixn.pos - target).normalize() + Vector3::new(1.0, 1.0, 1.0))
+                },
+                None => Vector3::new(0.1, 0.4, 0.1),
+            };
+        };
+
+        color / num_samples as f32
     }
 }
 
